940f4eabe1f498c724e0e98afe770006aa472b32
haoxz11 <haoxz11@gmail.com>
2026-09-15 17:59:51 +0800

    coral-cli 0.1.0（修复空文件）

---
+++ Formula/coral-cli.rb
@@ -0,0 +1,57 @@
+class CoralCli < Formula
+  desc "coral 入口：clap 参数、配置加载、tracing 初始化、生命周期与优雅停机"
+  homepage "https://github.com/haoxz11/coral"
+  version "0.1.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/haoxz11/coral/releases/download/v0.1.0/coral-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "7ad1ed6988d437e8f0baddc73f0f10565fd2d6be8f87b28dcb926c1a0bc6d056"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/haoxz11/coral/releases/download/v0.1.0/coral-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "f9523d06760cfd9ba83b29e833e3261a377fc636f2baf2c97f0be92dc21b9612"
+    end
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "coral"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "coral"
+    end
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
